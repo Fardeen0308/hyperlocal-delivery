f8f4504ad6f08727a38d0f1a29ef46c142ec9b8f
Fardeen0308 <fardeenkandanoor81@gmail.com>
2026-09-01 02:50:32 +0530

    updated Backend

--- backend/server2.js
+++ backend/server2.js
@@ -20,7 +20,7 @@ app.use(express.json());
 
 const supabase = createClient(
     process.env.SUPABASE_URL,
-    process.env.SUPABASE_ANON_KEY
+    process.env.SUPABASE_SERVICE_ROLE_KEY
 );
 
 cloudinary.config({
