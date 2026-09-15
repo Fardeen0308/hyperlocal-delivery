da3c3791683898c56661cc63d69010008983a4ee
Fardeen0308 <fardeenkandanoor81@gmail.com>
2026-09-15 16:43:54 +0530

    updated Frontend

--- auth.js
+++ auth.js
@@ -11,20 +11,38 @@ function requireRole(role) {
         return;
     }
 
-    if (user.role !== role) {
+    // Support one role or multiple roles
+    const allowedRoles =
+        Array.isArray(role) ? role : [role];
+
+    if (!allowedRoles.includes(user.role)) {
 
         alert("Access Denied");
 
         if (user.role === "customer") {
+
             window.location.href = "index.html";
+
         }
 
         else if (user.role === "admin") {
+
             window.location.href = "admin-dashboard.html";
+
         }
 
         else if (user.role === "delivery") {
+
             window.location.href = "delivery-dashboard.html";
+
         }
+
+        else if (user.role === "store_owner") {
+
+            window.location.href = "dashboard.html";
+
+        }
+
     }
+
 }
